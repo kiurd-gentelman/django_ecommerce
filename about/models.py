c4562e4f3745577a590e4da5841dba377a6bc486
Imtiaz Ur Rahman Khan <65729806+kiurd-gentelman@users.noreply.github.com>
2022-03-14 23:56:49 +0600

    about in admin panel

--- about/models.py
+++ about/models.py
@@ -4,8 +4,11 @@ from django.db import models
 # Create your models here.
 
 class About(models.Model):
-    title = models.CharField(max_length=200, blank='false')
+    title = models.CharField(max_length=200, blank=False)
     description = models.TextField()
     image = models.ImageField(upload_to='about/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
