2d7f3f1b8e162e3d6c3b442e5f7a263a12a47e7c
Imtiaz Ur Rahman Khan <65729806+kiurd-gentelman@users.noreply.github.com>
2022-03-28 02:43:33 +0600

    shop page setup

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from .models import Product, Category, Brand
+from pprint import pprint
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'shop/index.html')
+    brands = Brand.objects.all()
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    return render(request, 'shop/index.html', {'brands': brands, 'categories': categories, 'products': products})
