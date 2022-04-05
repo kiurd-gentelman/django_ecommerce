ce8ddcb8b2b5748c2efaf6c52375085a9e027638
Imtiaz Ur Rahman Khan <65729806+kiurd-gentelman@users.noreply.github.com>
2022-04-06 01:01:50 +0600

    add to cart complete and quantity update

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+import pdb
 
 from .models import Product, Category, Brand, Cart
 from pprint import pprint
@@ -19,10 +20,18 @@ def index(request):
 @login_required(login_url='/account/login/')
 def addToCart(request, id):
     product = Product.objects.get(id=id)
-    cart = Cart.objects.create(product_id=id, quantity=1, price=product.price, user_id=0)
+    cart = Cart.objects.filter(user=request.user, product=product)
+    if cart.exists():
+        # return HttpResponse(cart.quantity)
+        cart = cart.first()
+        cart.quantity = cart.quantity + 1
+        cart.save()
+    else:
+        Cart.objects.create(product_id=id, quantity=1, price=product.price, user_id=1)
     return redirect('shop.index')
 
 
+@login_required(login_url='/account/login/')
 def cartIndex(request):
     cart = Cart.objects.all()
     return render(request, 'cart/index.html', {'cart': cart})
